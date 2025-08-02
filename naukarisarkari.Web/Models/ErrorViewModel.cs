53250f2366e6be95306a2a40efbcad388dbb5e4a
kunal <kunal.wavhule@maxval.net>
2025-08-03 00:22:59 +0530

    Initial commit -ASP .NET Core MVC app

---
+++ naukarisarkari.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace naukarisarkari.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

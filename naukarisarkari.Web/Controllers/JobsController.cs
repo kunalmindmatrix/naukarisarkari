124473e99d2dbe27209c27f81eb21293520f502a
kunal <kunal.wavhule@maxval.net>
2025-08-03 00:31:10 +0530

    Creating the testing Jobs panel
    Creating the testing Jobs panel

---
+++ naukarisarkari.Web/Controllers/JobsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using naukarisarkari.Web.Models;
+
+namespace naukarisarkari.Web.Controllers
+{
+    public class JobsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Dummy job data for testing
+            var jobs = new List<Job>
+            {
+                new Job { Id = 1, Title = "Junior Clerk", Department = "MPSC", PostedOn = DateTime.Now.AddDays(-2), ApplyLink = "https://example.com/mpsc" },
+                new Job { Id = 2, Title = "Assistant Engineer", Department = "RRB", PostedOn = DateTime.Now.AddDays(-5), ApplyLink = "https://example.com/rrb" },
+                new Job { Id = 3, Title = "Bank PO", Department = "IBPS", PostedOn = DateTime.Now.AddDays(-1), ApplyLink = "https://example.com/ibps" }
+            };
+
+            return View(jobs);
+        }
+    }
+}
---
+++ naukarisarkari.Web/Models/Job.cs
@@ -0,0 +1,11 @@
+﻿namespace naukarisarkari.Web.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+        public DateTime PostedOn { get; set; }
+        public string ApplyLink { get; set; }
+    }
+}
